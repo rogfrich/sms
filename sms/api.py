5df06492f6e048b02265ed5ce2e87f07028b7ed6
Rich <richard.a.cairns@gmail.com>
2021-06-28 20:21:19 +0100

    format with black

--- sms/api.py
+++ sms/api.py
@@ -11,17 +11,17 @@ class SMSMessage:
     def __init__(self, body: str = ""):
         # Set SMS body text
         if not body:
-            self.body = f'Test message. Timestamp: {datetime.datetime.now()}'
+            self.body = f"Test message. Timestamp: {datetime.datetime.now()}"
         else:
             self.body = body
 
         # Get Twilio credentials from env variables
-        self.account_sid: str = os.getenv('TWILIO_ACCOUNT_SID')
-        self.auth_token: str = os.getenv('TWILIO_AUTH_TOKEN')
+        self.account_sid: str = os.getenv("TWILIO_ACCOUNT_SID")
+        self.auth_token: str = os.getenv("TWILIO_AUTH_TOKEN")
 
         # Set to and from numbers.
-        self.to_number: str = os.getenv('TWILIO_TO_NUMBER')
-        self.from_number: str = os.getenv('TWILIO_FROM_NUMBER')
+        self.to_number: str = os.getenv("TWILIO_TO_NUMBER")
+        self.from_number: str = os.getenv("TWILIO_FROM_NUMBER")
 
         self.sid = None  # Will be set upon successful sending of the message
 
@@ -33,9 +33,7 @@ class SMSMessage:
 
         # Submit the message to the Twilio API. This is the point where the SMS is actually sent
         message = client.messages.create(
-            body=self.body,
-            from_=self.from_number,
-            to=self.to_number
+            body=self.body, from_=self.from_number, to=self.to_number
         )
 
         # Twilio returns a SID which is a unique reference for a successfully sent SMS
